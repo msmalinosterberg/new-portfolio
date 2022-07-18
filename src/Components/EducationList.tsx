dae12de7adab9f55d233da7a5d4fbec79549ab8c
Malin Österberg <msmalinosterberg@gmail.com>
2022-07-18 15:55:23 +0200

    education in english

--- src/Components/EducationList.tsx
+++ src/Components/EducationList.tsx
@@ -3,42 +3,38 @@ export interface Education {
     title: string
     date: string
     description: string
-    // imageUrl: string
-    // link: string
-    // tags: string
-    // githubRepo: string
 }
 
 
 export const educationList: Education[] = [{
     id: 1,
-    title: 'Front end developer - Medieinstitutet Göteborg',
+    title: 'Front end developer - Medieinstitutet Gothenburg',
     date: '2020-2022',
-    description: 'HTML & CSS 20-yhp. Grundkurs JavaScript 60yhp. Projektarbete med agila metoder 15yhp. JavaScript fördjupningskurs 40yhp. Dynamisk webbutveckling 45yhp. Grafiska verktyg för gränssnittsdesign 15yhp. UX & Usability 15yhp. LIA 1 - 50yhp. Arbetsmetodik för utvecklare 20yhp. Kompentensportfölj och entreprenörsskap 10yhp. Examensarbete 30yhp. LIA 2 80yhp.',
+    description: 'HTML & CSS 20-yhp. Basic JavaScript 60yhp. Project management with agile methods 15yhp. Advanced JavaScript 40yhp. Dynamic web development 45yhp. Graphic tools for interface design 15yhp. UX & Usability 15yhp. Internship-1 (LIA)50yhp. Working methodology for developers 20yhp. Entrepreneurship 10yhp. Master theisis 30yhp. Internship-2 (LIA) 80yhp.',
 },
 {
     id: 2,
-    title: 'Distanskurs i copywriting - Forsbergs Stockholm ',
+    title: 'Remote course in copywriting - Forsbergs Stockholm ',
     date: '2019',
-    description: 'Identifiera tydliga målgrupper och anpassa budskapet och innehållet till dem. Öva på att tänka strategiskt, kreativt och konceptuellt och skapa budskap utifrån mina insikter. Jag skrev för sociala medier, print och i brevform.',
+    description: "Identify target groups and write for them in different medias (print/social media/letters). Practice thinking creatively, strategically and conceptually when creating campaigns.  ",
 },
 {
     id: 3,
-    title: 'Webbkommunikatör - Nordic Business Institute Göteborg',
+    title: 'Digital communication - Nordic Business Institute Gothenburg',
     date: '2014-2016',
-    description: ' Utbildningen behandlade allt från skrivande till strategisk kommunikation och har gett mig kompetens inom kommunikation, marknadsföring, sociala medier, digital produktion, SEO och journalistiskt skrivande. Vi arbetade i olika CMS (Wordpress, Drupal) program (t ex Adobe Premiere, Photoshop, Google Analytics m.m.) samt med dem största vedertagna sociala kanalerna.',
+    description: "Social media, copywriting, Adobe programs, project management, Google Analytics, paid ads, influence marketing and more. ",
 },
 {
     id: 4,
-    title: 'Fil.kand examen i Socialantropologi - Göteborgs Universitetet',
+    title: 'Bachelor degree in social anthropology - University of Gothenburg',
     date: '2008-2013',
-    description: 'Socialantroplogi 90hp, Humanekologi 60hp, Globala utvecklingsstudier 30hp',
+    description: 'Social anthropology 90hp, Human Ecology 60hp, Global development studies 30hp',
 },
 {
     id: 5,
-    title: 'Intensivkurs i projektledning - Upp!Göteborg / Club Global ',
+    title: 'Intensive course in project management - Upp!Göteborg / Club Global ',
     date: '2011',
-    description: 'Intensivkurs i projektledning med fokus på starta eget och ledarskap. Kursen gav mig kunskaper inom projektledning, ekonomi och budget samt ledarskap.',
+    description: 'The course was about leadership, how to start your own business, plan and organize your budget and more.',
 },
 ];
 
