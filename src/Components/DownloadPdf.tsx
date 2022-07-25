59d0b32c3a593b6f33840fa8c5bf98ee1d2bca28
Malin Österberg <msmalinosterberg@gmail.com>
2022-07-25 17:39:45 +0200

    fixed the bug with download pdf-file

--- src/Components/DownloadPdf.tsx
+++ src/Components/DownloadPdf.tsx
@@ -6,8 +6,8 @@ import download from '../Assets/downloadPdf.png'
 export default function DownloadPdf() {
     const saveFile = () => {
         saveAs(
-            "https://github.com/msmalinosterberg/new-portfolio/blob/7ea613ce1089a86ba37ced57bb49192de21e2d3d/CVMalinO%CC%88sterberg.pdf",
-            "CVMalinÖsterberg.pdf"
+            "https://drive.google.com/file/d/11yaBIRAReeVWmRIrwtbHST4prj9ccRw0/view?usp=sharing",
+            "CVMalinÖsterberg"
         );
     };
     return (
